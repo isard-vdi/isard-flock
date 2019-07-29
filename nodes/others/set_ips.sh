56195f039471b4b4dc08cd6266af612d5e2bf262
darta <info@isardvdi.com>
2019-07-29 10:07:44 +0200

    modify ip instead of removing

--- nodes/others/set_ips.sh
+++ nodes/others/set_ips.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# exit when any command fails
-set -e
-
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 remove_10g_ifs(){
 	nmcli con delete {nas,drbd}
 	rm -rf /etc/sysconfig/network-scripts/ifcfg-{nas,drbd}
@@ -16,7 +8,7 @@ remove_10g_ifs(){
 update_ifs(){
 	for new_if in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
 	do
-		if [[ $new_if == "drbd" ]] || [[ $new_if == "drbd" ]] ; then
+		if [[ $new_if == "nas" ]] || [[ $new_if == "drbd" ]] ; then
 			set_if
 		fi
 	done
@@ -25,18 +17,16 @@ update_ifs(){
 set_if(){
 	net=0
 	if [[ $new_if == "drbd" ]]; then net=1; fi
-	nmcli con add con-name "$new_if" ifname $old_if type ethernet ip4 172.31.$net.$(($host+10))/24
-	nmcli con mod "$new_if" ipv6.method ignore
-	nmcli con mod "$new_if" 802-3-ethernet.mtu 9000
-	MAC=$(cat /sys/class/net/$new_if/address)
-	echo -n 'HWADDR="'$MAC\" >> /etc/sysconfig/network-scripts/ifcfg-$new_if
-	ip link set $new_if down
-	ip link set $new_if mtu 9000
-	ip link set $new_if up
+	nmcli con mod "$new_if" ipv4.addresses 172.31.$net.$(($host+10))/24
+	nmcli dev disconnect "$new_if"
+	nmcli con up "$new_if"
+	#~ ip link set $new_if down
+	#~ ip link set $new_if mtu 9000
+	#~ ip link set $new_if up
 }
 
 host=$1
 echo "if$host" > /etc/hostname
 sysctl -w kernel.hostname=if$host
-remove_10g_ifs
+#~ remove_10g_ifs
 update_ifs
